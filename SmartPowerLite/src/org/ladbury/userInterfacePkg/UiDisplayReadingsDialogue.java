6a45d1214f5a838a8af4cbd189923d32979fff34
gjwo <grahamjwood@gmail.com>
2017-09-07 21:15:08 +0100

    reading dialog working

--- SmartPowerLite/src/org/ladbury/userInterfacePkg/UiDisplayReadingsDialogue.java
+++ SmartPowerLite/src/org/ladbury/userInterfacePkg/UiDisplayReadingsDialogue.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -23,16 +24,12 @@ public class UiDisplayReadingsDialogue extends JDialog
     private static final JLabel lblLatest    =   new JLabel("  End Time:");
     private final JComboBox<DataServiceMeter> comboMeter;
     private final JComboBox<DataServiceMetric> comboMetric;
-    private final JComboBox<String> comboEarliest;
-    private final JComboBox<String> comboLatest;
-    //private final Collection<String> earliestTimes;
-    //private final Collection<String> latestTimes;
     private Collection<String> readings;
     private final JButton btnOK = new JButton("OK");
     private DataServiceMeter meter;
     private DataServiceMetric metric;
-    private String earliestTime;
-    private String latestTime;
+    private Date earliestTime;
+    private Date latestTime;
 
     UiDisplayReadingsDialogue(String title )
     {
@@ -47,8 +44,6 @@ public class UiDisplayReadingsDialogue extends JDialog
         Collection<String> readings = SmartPower.getMain().getDataService().getDBResourceForPeriodAsStrings(
                 meter.getTag()+"/"+metric.getTag(), "2017-09-03 11:02:00","2017-09-06 11:03:01");
 
-        if( readings.iterator().hasNext()) earliestTime = readings.iterator().next();
-        if( readings.iterator().hasNext()) latestTime = readings.iterator().next();
 
         //Set up combo boxes and add a listener for changes
         comboMeter  =   new JComboBox<>(meters.toArray(new DataServiceMeter[meters.size()]));
@@ -63,19 +58,6 @@ public class UiDisplayReadingsDialogue extends JDialog
         });
         //comboMetric.addItemListener(comboListener);
 
-        comboEarliest = new JComboBox<>(readings.toArray(new String[readings.size()]));
-        comboEarliest.addActionListener(event -> {
-            earliestTime = (String)comboEarliest.getSelectedItem();
-        });
-        //comboEarliest.addItemListener(comboListener);
-
-        comboLatest =   new JComboBox<>(readings.toArray(new String[readings.size()]));
-        comboLatest.addActionListener(event -> {
-            latestTime = (String)comboLatest.getSelectedItem();
-        });
-        //comboLatest .addItemListener(comboListener);
-
-
         JPanel panel1 = new JPanel(new GridLayout(2,4));
         panel1.add(lblMeter);
         panel1.add(comboMeter);
@@ -86,12 +68,16 @@ public class UiDisplayReadingsDialogue extends JDialog
         JSpinner.DateEditor earliestTimeEditor = new JSpinner.DateEditor(earliestTimeSpinner, "d MMM yyyy HH:mm:ss");
         earliestTimeSpinner.setEditor(earliestTimeEditor);
         earliestTimeSpinner.setValue(new Date()); // will only show the current time
-        earliestTimeSpinner.addChangeListener(e -> System.out.println(earliestTimeSpinner.getValue()));
+        earliestTimeSpinner.addChangeListener(e -> earliestTime = (Date)earliestTimeSpinner.getValue());
+
         JSpinner latestTimeSpinner = new JSpinner( new SpinnerDateModel() );
         JSpinner.DateEditor latestTimeEditor = new JSpinner.DateEditor(latestTimeSpinner, "d MMM yyyy HH:mm:ss");
         latestTimeSpinner.setEditor(latestTimeEditor);
         latestTimeSpinner.setValue(new Date()); // will only show the current time
-        latestTimeSpinner.addChangeListener(e -> System.out.println(latestTimeSpinner.getValue()));
+        latestTimeSpinner.addChangeListener(e -> latestTime = (Date)latestTimeSpinner.getValue());
+
+        earliestTime = new Date();
+        latestTime = new Date();
 
         panel1.add(lblEarliest);
         panel1.add(earliestTimeSpinner);
@@ -110,8 +96,9 @@ public class UiDisplayReadingsDialogue extends JDialog
 
         btnOK.addActionListener(event ->
         {
-            System.out.println("Button Pressed");
-            SmartPower.getMain().getDataService().printDBResourceForPeriod(meter.getTag()+"/"+metric.getTag(),earliestTime,latestTime);
+            //System.out.println("Button Pressed");
+            final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            SmartPower.getMain().getDataService().printDBResourceForPeriod(meter.getTag()+"/"+metric.getTag(),sdf.format(earliestTime),sdf.format(latestTime));
         });
 
         this.add(btnOK, BorderLayout.SOUTH);
